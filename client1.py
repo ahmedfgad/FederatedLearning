28066d5cda85c032baf84c4a884b682e2740ddbe
Ahmed Gad <ahmed.f.gad@gmail.com>
2020-06-30 20:50:57 +0200

    Add files via upload

--- client1.py
+++ client1.py
@@ -1,15 +1,98 @@
 import socket
 import pickle
 import numpy
+import threading
 
 import pygad
-import pygad.nn
-import pygad.gann
+import nn
+import gann
+
+import kivy.app
+import kivy.uix.button
+import kivy.uix.label
+import kivy.uix.boxlayout
+import kivy.uix.textinput
+
+class ClientApp(kivy.app.App):
+    
+    def __init__(self):
+        super().__init__()
+
+    def create_socket(self, *args):
+        self.soc = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        self.label.text = "Socket Created"
+
+        self.create_socket_btn.disabled = True
+        self.connect_btn.disabled = False
+        self.close_socket_btn.disabled = False
+
+    def connect(self, *args):
+        try:
+            self.soc.connect((self.server_ip.text, int(self.server_port.text)))
+            self.label.text = "Successful Connection to the Server"
+    
+            self.connect_btn.disabled = True
+            self.recv_train_model_btn.disabled = False
+
+        except BaseException as e:
+            self.label.text = "Error Connecting to the Server"
+            print("Error Connecting to the Server: {msg}".format(msg=e))
+
+            self.connect_btn.disabled = False
+            self.recv_train_model_btn.disabled = True
+
+    def recv_train_model(self, *args):
+        global GANN_instance
+
+        self.recv_train_model_btn.disabled = True
+        recvThread = RecvThread(kivy_app=self, buffer_size=1024, recv_timeout=10)
+        recvThread.start()
+
+    def close_socket(self, *args):
+        self.soc.close()
+        self.label.text = "Socket Closed"
+
+        self.create_socket_btn.disabled = False
+        self.connect_btn.disabled = True
+        self.recv_train_model_btn.disabled = True
+        self.close_socket_btn.disabled = True
+
+    def build(self):
+        self.create_socket_btn = kivy.uix.button.Button(text="Create Socket")
+        self.create_socket_btn.bind(on_press=self.create_socket)
+
+        self.server_ip = kivy.uix.textinput.TextInput(hint_text="Server IPv4 Address", text="localhost")
+        self.server_port = kivy.uix.textinput.TextInput(hint_text="Server Port Number", text="10000")
+
+        self.server_info_boxlayout = kivy.uix.boxlayout.BoxLayout(orientation="horizontal")
+        self.server_info_boxlayout.add_widget(self.server_ip)
+        self.server_info_boxlayout.add_widget(self.server_port)
+
+        self.connect_btn = kivy.uix.button.Button(text="Connect to Server", disabled=True)
+        self.connect_btn.bind(on_press=self.connect)
+
+        self.recv_train_model_btn = kivy.uix.button.Button(text="Receive & Train Model", disabled=True)
+        self.recv_train_model_btn.bind(on_press=self.recv_train_model)
+
+        self.close_socket_btn = kivy.uix.button.Button(text="Close Socket", disabled=True)
+        self.close_socket_btn.bind(on_press=self.close_socket)
+
+        self.label = kivy.uix.label.Label(text="Socket Status")
+
+        self.box_layout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
+        self.box_layout.add_widget(self.create_socket_btn)
+        self.box_layout.add_widget(self.server_info_boxlayout)
+        self.box_layout.add_widget(self.connect_btn)
+        self.box_layout.add_widget(self.recv_train_model_btn)
+        self.box_layout.add_widget(self.close_socket_btn)
+        self.box_layout.add_widget(self.label)
+
+        return self.box_layout
 
 def fitness_func(solution, sol_idx):
     global GANN_instance, data_inputs, data_outputs
 
-    predictions = pygad.nn.predict(last_layer=GANN_instance.population_networks[sol_idx],
+    predictions = nn.predict(last_layer=GANN_instance.population_networks[sol_idx],
                                            data_inputs=data_inputs)
     correct_predictions = numpy.where(predictions == data_outputs)[0].size
     solution_fitness = (correct_predictions/data_outputs.size)*100
@@ -19,21 +102,21 @@ def fitness_func(solution, sol_idx):
 def callback_generation(ga_instance):
     global GANN_instance, last_fitness
 
-    population_matrices = pygad.gann.population_as_matrices(population_networks=GANN_instance.population_networks, 
+    population_matrices = gann.population_as_matrices(population_networks=GANN_instance.population_networks, 
                                                             population_vectors=ga_instance.population)
 
     GANN_instance.update_population_trained_weights(population_trained_weights=population_matrices)
 
-    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
+#    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+#    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
+#    print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
 
-last_fitness = 0
+#last_fitness = 0
 
 def prepare_GA(GANN_instance):
     # population does not hold the numerical weights of the network instead it holds a list of references to each last layer of each network (i.e. solution) in the population. A solution or a network can be used interchangeably.
     # If there is a population with 3 solutions (i.e. networks), then the population is a list with 3 elements. Each element is a reference to the last layer of each network. Using such a reference, all details of the network can be accessed.
-    population_vectors = pygad.gann.population_as_vectors(population_networks=GANN_instance.population_networks)
+    population_vectors = gann.population_as_vectors(population_networks=GANN_instance.population_networks)
     
     # To prepare the initial population, there are 2 ways:
     # 1) Prepare it yourself and pass it to the initial_population parameter. This way is useful when the user wants to start the genetic algorithm with a custom initial population.
@@ -80,80 +163,82 @@ data_inputs = numpy.array([[0, 1],
 data_outputs = numpy.array([1, 
                             0])
 
-def recv(soc, buffer_size=1024, recv_timeout=10):
-    received_data = b""
-    while str(received_data)[-2] != '.':
+class RecvThread(threading.Thread):
+
+    def __init__(self, kivy_app, buffer_size, recv_timeout):
+        threading.Thread.__init__(self)
+        self.kivy_app = kivy_app
+        self.buffer_size = buffer_size
+        self.recv_timeout = recv_timeout
+
+    def recv(self):
+        received_data = b""
+        while str(received_data)[-2] != '.':
+            try:
+                self.kivy_app.soc.settimeout(self.recv_timeout)
+                received_data += self.kivy_app.soc.recv(self.buffer_size)
+            except socket.timeout:
+                print("A socket.timeout exception occurred because the server did not send any data for {recv_timeout} seconds.".format(recv_timeout=self.recv_timeout))
+                self.kivy_app.label.text = "{recv_timeout} Seconds of Inactivity. socket.timeout Exception Occurred".format(recv_timeout=self.recv_timeout)
+                return None, 0
+            except BaseException as e:
+                return None, 0
+                print("Error While Receiving Data from the Server: {msg}.".format(msg=e))
+                self.kivy_app.label.text = "Error While Receiving Data from the Server"
+
         try:
-            soc.settimeout(recv_timeout)
-            received_data += soc.recv(buffer_size)
-        except socket.timeout:
-            print("A socket.timeout exception occurred because the server did not send any data for {recv_timeout} seconds.".format(recv_timeout=recv_timeout))
-            return None, 0
+            received_data = pickle.loads(received_data)
         except BaseException as e:
+            print("Error Decoding the Client's Data: {msg}.\n".format(msg=e))
+            self.kivy_app.label.text = "Error Decoding the Client's Data"
             return None, 0
-            print("An error occurred while receiving data from the server {msg}.".format(msg=e))
-
-    try:
-        received_data = pickle.loads(received_data)
-    except BaseException as e:
-        print("Error Decoding the Client's Data: {msg}.\n".format(msg=e))
-        return None, 0
-
-    return received_data, 1
-
-soc = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-print("Socket Created.\n")
-
-try:
-    soc.connect(("localhost", 10000))
-except BaseException as e:
-    print("Error Connecting to the Server: {msg}".format(msg=e))
-    soc.close()
-    print("Socket Closed.")
-
-print("Successful Connection to the Server.\n")
-
-subject = "echo"
-GANN_instance = None
-best_sol_idx = -1
-
-while True:
-    data = {"subject": subject, "data": GANN_instance, "best_solution_idx": best_sol_idx}
-    data_byte = pickle.dumps(data)
     
-    print("Sending the Model to the Server.\n")
-    soc.sendall(data_byte)
-    
-    print("Receiving Reply from the Server.")
-    received_data, status = recv(soc=soc, 
-                                 buffer_size=1024, 
-                                 recv_timeout=10)
-    if status == 0:
-        print("Nothing Received from the Server.")
-        break
-    else:
-        print(received_data, end="\n\n")
+        return received_data, 1
 
-    subject = received_data["subject"]
-    if subject == "model":
-        GANN_instance = received_data["data"]
-    elif subject == "done":
-        print("Model is trained.")
-        break
-    else:
-        print("Unrecognized message type.")
-        break
+    def run(self):
+        global GANN_instance
 
-    ga_instance = prepare_GA(GANN_instance)
+        subject = "echo"
+        GANN_instance = None
+        best_sol_idx = -1
 
-    ga_instance.run()
+        while True:
+            data = {"subject": subject, "data": GANN_instance, "best_solution_idx": best_sol_idx}
+            data_byte = pickle.dumps(data)
 
-    ga_instance.plot_result()
+            self.kivy_app.label.text = "Sending a Message of Type {subject} to the Server".format(subject=subject)
+            try:
+                self.kivy_app.soc.sendall(data_byte)
+            except BaseException as e:
+                self.kivy_app.label.text = "Error Connecting to the Server. The server might has been closed."
+                print("Error Connecting to the Server: {msg}".format(msg=e))
+                break
 
-    subject = "model"
-    best_sol_idx = ga_instance.best_solution()[2]
+            self.kivy_app.label.text = "Receiving Reply from the Server"
+            received_data, status = self.recv()
+            if status == 0:
+                self.kivy_app.label.text = "Nothing Received from the Server"
+                break
+            else:
+                self.kivy_app.label.text = "New Message from the Server"
 
-# predictions = pygad.nn.predict(last_layer=GANN_instance.population_networks[best_sol_idx], data_inputs=data_inputs)
+            subject = received_data["subject"]
+            if subject == "model":
+                GANN_instance = received_data["data"]
+            elif subject == "done":
+                self.kivy_app.label.text = "Model is Trained"
+                break
+            else:
+                self.kivy_app.label.text = "Unrecognized Message Type: {subject}".format(subject=subject)
+                break
 
-soc.close()
-print("Socket Closed.\n")
+            ga_instance = prepare_GA(GANN_instance)
+
+            ga_instance.run()
+
+            subject = "model"
+            best_sol_idx = ga_instance.best_solution()[2]
+
+clientApp = ClientApp()
+clientApp.title = "Client App"
+clientApp.run()
--- server.py
+++ server.py
@@ -2,11 +2,89 @@ import socket
 import pickle
 import threading
 import time
-
-import pygad
-import pygad.nn
-import pygad.gann
 import numpy
+
+import nn
+import gann
+
+import kivy.app
+import kivy.uix.button
+import kivy.uix.label
+import kivy.uix.textinput
+import kivy.uix.boxlayout
+
+class ServerApp(kivy.app.App):
+    
+    def __init__(self):
+        super().__init__()
+
+    def create_socket(self, *args):
+        self.soc = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        self.label.text = "Socket Created"
+
+        self.create_socket_btn.disabled = True
+        self.bind_btn.disabled = False
+        self.close_socket_btn.disabled = False
+
+    def bind_socket(self, *args):
+        ipv4_address = self.server_ip.text
+        port_number = self.server_port.text
+        self.soc.bind((ipv4_address, int(port_number)))
+        self.label.text = "Socket Bound to IPv4 & Port Number"
+
+        self.bind_btn.disabled = True
+        self.listen_btn.disabled = False
+
+    def listen_accept(self, *args):
+        self.soc.listen(1)
+        self.label.text = "Socket is Listening for Connections"
+
+        self.listen_btn.disabled = True
+
+        self.listenThread = ListenThread(kivy_app=self)
+        self.listenThread.start()
+
+    def close_socket(self, *args):
+        self.soc.close()
+        self.label.text = "Socket Closed"
+
+        self.create_socket_btn.disabled = False
+        self.bind_btn.disabled = True
+        self.listen_btn.disabled = True
+        self.close_socket_btn.disabled = True
+
+    def build(self):
+        self.create_socket_btn = kivy.uix.button.Button(text="Create Socket", disabled=False)
+        self.create_socket_btn.bind(on_press=self.create_socket)
+
+        self.server_ip = kivy.uix.textinput.TextInput(hint_text="IPv4 Address", text="localhost")
+        self.server_port = kivy.uix.textinput.TextInput(hint_text="Port Number", text="10000")
+
+        self.server_socket_box_layout = kivy.uix.boxlayout.BoxLayout(orientation="horizontal")
+        self.server_socket_box_layout.add_widget(self.server_ip)
+        self.server_socket_box_layout.add_widget(self.server_port)
+
+        self.bind_btn = kivy.uix.button.Button(text="Bind Socket", disabled=True)
+        self.bind_btn.bind(on_press=self.bind_socket)
+
+        self.listen_btn = kivy.uix.button.Button(text="Listen to Connections", disabled=True)
+        self.listen_btn.bind(on_press=self.listen_accept)
+
+        self.close_socket_btn = kivy.uix.button.Button(text="Close Socket", disabled=True)
+        self.close_socket_btn.bind(on_press=self.close_socket)
+
+        self.label = kivy.uix.label.Label(text="Socket Status")
+
+        self.box_layout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
+
+        self.box_layout.add_widget(self.create_socket_btn)
+        self.box_layout.add_widget(self.server_socket_box_layout)
+        self.box_layout.add_widget(self.bind_btn)
+        self.box_layout.add_widget(self.listen_btn)
+        self.box_layout.add_widget(self.close_socket_btn)
+        self.box_layout.add_widget(self.label)
+
+        return self.box_layout
 
 model = None
 
@@ -26,7 +104,7 @@ num_classes = 2
 num_inputs = 2
 
 num_solutions = 6
-GANN_instance = pygad.gann.GANN(num_solutions=num_solutions,
+GANN_instance = gann.GANN(num_solutions=num_solutions,
                                 num_neurons_input=num_inputs,
                                 num_neurons_hidden_layers=[2],
                                 num_neurons_output=num_classes,
@@ -35,12 +113,13 @@ GANN_instance = pygad.gann.GANN(num_solutions=num_solutions,
 
 class SocketThread(threading.Thread):
 
-    def __init__(self, connection, client_info, buffer_size=1024, recv_timeout=5):
+    def __init__(self, connection, client_info, kivy_app, buffer_size=1024, recv_timeout=5):
         threading.Thread.__init__(self)
         self.connection = connection
         self.client_info = client_info
         self.buffer_size = buffer_size
         self.recv_timeout = recv_timeout
+        self.kivy_app = kivy_app
 
     def recv(self):
         received_data = b""
@@ -58,6 +137,7 @@ class SocketThread(threading.Thread):
 
                 elif str(data)[-2] == '.':
                     print("All data ({data_len} bytes) Received from {client_info}.".format(client_info=self.client_info, data_len=len(received_data)))
+                    self.kivy_app.label.text = "All data ({data_len} bytes) Received from {client_info}.".format(client_info=self.client_info, data_len=len(received_data))
 
                     if len(received_data) > 0:
                         try:
@@ -68,6 +148,7 @@ class SocketThread(threading.Thread):
 
                         except BaseException as e:
                             print("Error Decoding the Client's Data: {msg}.\n".format(msg=e))
+                            self.kivy_app.label.text = "Error Decoding the Client's Data"
                             return None, 0
 
                 else:
@@ -76,15 +157,16 @@ class SocketThread(threading.Thread):
 
             except BaseException as e:
                 print("Error Receiving Data from the Client: {msg}.\n".format(msg=e))
+                self.kivy_app.label.text = "Error Receiving Data from the Client"
                 return None, 0
 
     def model_averaging(self, model, other_model):
-        model_weights = pygad.nn.layers_weights(last_layer=model, initial=False)
-        other_model_weights = pygad.nn.layers_weights(last_layer=other_model, initial=False)
+        model_weights = nn.layers_weights(last_layer=model, initial=False)
+        other_model_weights = nn.layers_weights(last_layer=other_model, initial=False)
         
         new_weights = (model_weights + other_model_weights)/2
 
-        pygad.nn.update_layers_trained_weights(last_layer=model, final_weights=new_weights)
+        nn.update_layers_trained_weights(last_layer=model, final_weights=new_weights)
 
     def reply(self, received_data):
         global GANN_instance, data_inputs, data_outputs, model
@@ -92,14 +174,27 @@ class SocketThread(threading.Thread):
             if (("data" in received_data.keys()) and ("subject" in received_data.keys())):
                 subject = received_data["subject"]
                 print("Client's Message Subject is {subject}.".format(subject=subject))
+                self.kivy_app.label.text = "Client's Message Subject is {subject}".format(subject=subject)
 
                 print("Replying to the Client.")
+                self.kivy_app.label.text = "Replying to the Client"
                 if subject == "echo":
-                    try:
+                    if model is None:
                         data = {"subject": "model", "data": GANN_instance}
+                    else:
+                        predictions = nn.predict(last_layer=model, data_inputs=data_inputs)
+                        error = numpy.sum(numpy.abs(predictions - data_outputs))
+                        # In case a client sent a model to the server despite that the model error is 0.0. In this case, no need to make changes in the model.
+                        if error == 0:
+                            data = {"subject": "done", "data": None}
+                        else:
+                            data = {"subject": "model", "data": GANN_instance}
+
+                    try:
                         response = pickle.dumps(data)
                     except BaseException as e:
-                        print("Error Decoding the Client's Data: {msg}.\n".format(msg=e))
+                        print("Error Encoding the Message: {msg}.\n".format(msg=e))
+                        self.kivy_app.label.text = "Error Encoding the Message"
                 elif subject == "model":
                     try:
                         GANN_instance = received_data["data"]
@@ -109,7 +204,7 @@ class SocketThread(threading.Thread):
                         if model is None:
                             model = best_model
                         else:
-                            predictions = pygad.nn.predict(last_layer=model, data_inputs=data_inputs)
+                            predictions = nn.predict(last_layer=model, data_inputs=data_inputs)
     
                             error = numpy.sum(numpy.abs(predictions - data_outputs))
     
@@ -124,11 +219,12 @@ class SocketThread(threading.Thread):
                         # print(best_model.trained_weights)
                         # print(model.trained_weights)
 
-                        predictions = pygad.nn.predict(last_layer=model, data_inputs=data_inputs)
+                        predictions = nn.predict(last_layer=model, data_inputs=data_inputs)
                         print("Model Predictions: {predictions}".format(predictions=predictions))
 
                         error = numpy.sum(numpy.abs(predictions - data_outputs))
-                        print("Error = {error}".format(error=error))
+                        print("Prediction Error = {error}".format(error=error))
+                        self.kivy_app.label.text = "Prediction Error = {error}".format(error=error)
 
                         if error != 0:
                             data = {"subject": "model", "data": GANN_instance}
@@ -139,6 +235,7 @@ class SocketThread(threading.Thread):
 
                     except BaseException as e:
                         print("Error Decoding the Client's Data: {msg}.\n".format(msg=e))
+                        self.kivy_app.label.text = "Error Decoding the Client's Data"
                 else:
                     response = pickle.dumps("Response from the Server")
                             
@@ -146,14 +243,18 @@ class SocketThread(threading.Thread):
                     self.connection.sendall(response)
                 except BaseException as e:
                     print("Error Sending Data to the Client: {msg}.\n".format(msg=e))
+                    self.kivy_app.label.text = "Error Sending Data to the Client: {msg}".format(msg=e)
 
             else:
                 print("The received dictionary from the client must have the 'subject' and 'data' keys available. The existing keys are {d_keys}.".format(d_keys=received_data.keys()))
+                self.kivy_app.label.text = "Error Parsing Received Dictionary"
         else:
             print("A dictionary is expected to be received from the client but {d_type} received.".format(d_type=type(received_data)))
+            self.kivy_app.label.text = "A dictionary is expected but {d_type} received.".format(d_type=type(received_data))
 
     def run(self):
         print("Running a Thread for the Connection with {client_info}.".format(client_info=self.client_info))
+        self.kivy_app.label.text = "Running a Thread for the Connection with {client_info}.".format(client_info=self.client_info)
 
         # This while loop allows the server to wait for the client to send data more than once within the same connection.
         while True:
@@ -164,35 +265,38 @@ class SocketThread(threading.Thread):
             received_data, status = self.recv()
             if status == 0:
                 self.connection.close()
+                self.kivy_app.label.text = "Connection Closed with {client_info}".format(client_info=self.client_info)
                 print("Connection Closed with {client_info} either due to inactivity for {recv_timeout} seconds or due to an error.".format(client_info=self.client_info, recv_timeout=self.recv_timeout), end="\n\n")
                 break
 
             # print(received_data)
             self.reply(received_data)
 
-soc = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-print("Socket Created.\n")
+class ListenThread(threading.Thread):
 
-# Timeout after which the socket will be closed.
-# soc.settimeout(5)
+    def __init__(self, kivy_app):
+        threading.Thread.__init__(self)
+        self.kivy_app = kivy_app
 
-soc.bind(("localhost", 10000))
-print("Socket Bound to IPv4 Address & Port Number.\n")
+    def run(self):
+        while True:
+            try:
+                connection, client_info = self.kivy_app.soc.accept()
+                self.kivy_app.label.text = "New Connection from {client_info}".format(client_info=client_info)
+                socket_thread = SocketThread(connection=connection,
+                                             client_info=client_info, 
+                                             kivy_app=self.kivy_app,
+                                             buffer_size=1024,
+                                             recv_timeout=10)
+                socket_thread.start()
+            except BaseException as e:
+                self.kivy_app.soc.close()
+                print(e)
+                self.kivy_app.label.text = "Socket is No Longer Accepting Connections"
+                self.kivy_app.create_socket_btn.disabled = False
+                self.kivy_app.close_socket_btn.disabled = True
+                break
 
-soc.listen(1)
-print("Socket is Listening for Connections ....\n")
-
-all_data = b""
-while True:
-    try:
-        connection, client_info = soc.accept()
-        print("New Connection from {client_info}.".format(client_info=client_info))
-        socket_thread = SocketThread(connection=connection,
-                                     client_info=client_info, 
-                                     buffer_size=1024,
-                                     recv_timeout=10)
-        socket_thread.start()
-    except:
-        soc.close()
-        print("(Timeout) Socket Closed Because no Connections Received.\n")
-        break
+serverApp = ServerApp()
+serverApp.title="Server App"
+serverApp.run()
